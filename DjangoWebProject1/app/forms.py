38fde9910c6a60faaddd1a4c75fb2fcf79690bb4
Eugeneus404 <evgenypostanogov@yandex.ru>
2024-04-17 17:35:37 +0300

    Добавьте файлы проекта.

---
+++ DjangoWebProject1/app/forms.py
@@ -0,0 +1,38 @@
+﻿"""
+Definition of forms.
+"""
+
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+from django.utils.translation import gettext_lazy as _
+
+class BootstrapAuthenticationForm(AuthenticationForm):
+    """Authentication form which uses boostrap CSS."""
+    username = forms.CharField(max_length=254,
+                               widget=forms.TextInput({
+                                   'class': 'form-control',
+                                   'placeholder': 'Имя пользователя'}))
+    password = forms.CharField(label=_("Password"),
+                               widget=forms.PasswordInput({
+                                   'class': 'form-control',
+                                   'placeholder':'Пароль'}))
+    
+    
+class AnketaForm(forms.Form):
+    name = forms.CharField(label='Имя', max_length=100)
+    city = forms.CharField(label='Город', max_length=100)
+    job = forms.CharField(label='Специальность', max_length=100)
+    gender_choices = [
+        ('1', 'Мужской'),
+        ('2', 'Женский'),
+    ]
+    gender = forms.ChoiceField(label='Пол', choices=gender_choices, initial='male', widget=forms.RadioSelect)
+    internet_choices = [
+        ('1', 'Редко'),
+        ('2', 'Иногда'),
+        ('3', 'Часто'),
+    ]
+    internet = forms.ChoiceField(label='Частота использования интернета', choices=internet_choices, initial='often')
+    email = forms.EmailField(label='E-mail', max_length=100)
+    notice = forms.BooleanField(label='Получать новости сайта', required=False)
+    message = forms.CharField(label='Короткое резюме', widget=forms.Textarea)
---
+++ DjangoWebProject1/app/views.py
@@ -0,0 +1,104 @@
+﻿"""
+Definition of views.
+"""
+
+from datetime import datetime
+from django.shortcuts import render, redirect
+from django.http import HttpRequest
+from .forms import AnketaForm
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/index.html',
+        {
+            'title':'Главная',
+            'year':datetime.now().year,
+        }
+    )
+
+def contact(request):
+    """Renders the contact page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/contact.html',
+        {
+            'title':'Контакты',
+            'message':'Страница с нашими контактами',
+            'year':datetime.now().year,
+        }
+    )
+
+def about(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/about.html',
+        {
+            'title':'О нас',
+            'message':'Сведения о нас',
+            'year':datetime.now().year,
+        }
+    )
+
+
+def anketa(request):
+    assert isinstance(request, HttpRequest)
+    
+    gender = {'1': 'Мужчина', '2': 'Женщина'}
+    internet = {'1': 'Каждый день', '2': 'Несколько раз в день', '3': 'Несколько раз в неделю', '4': 'Несколько раз в месяц'}
+
+    if request.method == 'POST':
+        form = AnketaForm(request.POST)
+        if form.is_valid():
+            data = {
+                'name': form.cleaned_data['name'],
+                'city': form.cleaned_data['city'],
+                'job': form.cleaned_data['job'],
+                'gender': gender.get(form.cleaned_data['gender'], ''),
+                'internet': internet.get(form.cleaned_data['internet'], ''),
+                'notice': 'Да' if form.cleaned_data['notice'] else 'Нет',
+                'email': form.cleaned_data['email'],
+                'message': form.cleaned_data['message'],
+            }
+        else:
+            data = None
+    else:
+        form = AnketaForm()
+        data = None
+    
+    return render(request, 'app/anketa.html', {'form': form, 'data': data})
+
+def registration(request):
+    """Renders the registration page."""
+    if request.method == "POST":
+        regform = UserCreationForm(request.POST)
+        if regform.is_valid():
+            reg_f = regform.save(commit=False)
+            reg_f.is_staff = False
+            reg_f.is_active = True
+            reg_f.is_superuser = False
+            reg_f.date_joined = datetime.now()
+            regform.last_login = datetime.now()
+            
+            regform.save()
+            
+            return redirect("home")
+    else:
+        regform = UserCreationForm()
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        "app/registration.html",
+        {
+            'regform': regform,
+            'year': datetime.now().year,
+            'title':'Регистрация',
+        }
+    )
